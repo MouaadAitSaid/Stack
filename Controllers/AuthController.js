7a103b82fac10c67442d821787d8719ca777f29b
Mouaad <mouad.aitsaid.fycomputing@gmail.com>
2018-10-20 21:32:01 +0100

    stack v2, adding Users Logic  Done.

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -13,33 +13,55 @@ routes = [
         middleware: [function (req, res) {
             _u.PrintReq(req, true);
             try {
-                return User.findOne({username: req.body.username}).select('+password').exec((err, user) => {
-                    if (err) return Response.build(res, 508, {status: false, message: "Oups, something went wrong"});
-                    if (!user) return Response.build(res, 200, {status: true, message: "No User Found", data: null});
-                    return _u.comparePasswords(user.password, req.body.password, (isGoodPassword) => {
-                        if (!isGoodPassword) return Response.build(res, 200, {
-                            status: true,
-                            message: "User found, Wrong password",
-                            data: null
-                        });
-                        user.password = null;
-                        user.salt = null;
-                        _u.getToken({
-                            firstName: user.firstName,
-                            email: user.email,
-                            lastName: user.lastName,
-                            role: user.role,
-                            otherInfos: user.otherInfos
-                        }, (token) => {
+                switch (req.body.username) {
+                    case global.gConfig.sAdmin.username :
+                        if(global.gConfig.sAdmin.password!==req.body.password){
                             return Response.build(res, 200, {
                                 status: true,
-                                message: "Login Done. Welcome :) .",
+                                message: "Invalid credentiels",
+                                data: null
+                            });
+                        }
+                        let saData = global.gConfig.sAdmin;
+                        saData.username = null;
+                        saData.password = null;
+                        return _u.getToken(saData, (token) => {
+                            return Response.build(res, 200, {
+                                status: true,
+                                message: "Login Done. Welcome Super Admin. :) .",
                                 token: token
                             });
                         });
+                    default :
+                        return User.findOne({username: req.body.username}).select('+password').exec((err, user) => {
+                            if (err) return Response.build(res, 508, {status: false, message: "Oups, something went wrong"});
+                            if (!user) return Response.build(res, 200, {status: true, message: "Invalid credentiels", data: null});
+                            return _u.comparePasswords(user.password, req.body.password, (isGoodPassword) => {
+                                if (!isGoodPassword) return Response.build(res, 200, {
+                                    status: true,
+                                    message: "Invalid credentiels",
+                                    data: null
+                                });
+                                user.password = null;
+                                user.salt = null;
+                                _u.getToken({
+                                    firstName: user.firstName,
+                                    email: user.email,
+                                    lastName: user.lastName,
+                                    role: user.role,
+                                    otherInfos: user.otherInfos
+                                }, (token) => {
+                                    return Response.build(res, 200, {
+                                        status: true,
+                                        message: "Login Done. Welcome :) .",
+                                        token: token
+                                    });
+                                });
 
-                    })
-                })
+                            })
+                        })
+                }
+
             } catch (e) {
                 _u.console("e", true, e);
                 return Response.build(res, 508, {status: false, message: "Oups, something went wrong"});
